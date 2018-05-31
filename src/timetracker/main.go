9fdbcd7413b979a6b09bbc498f8d6240c7aec3ce
Nate Bosscher <nate@runsandbox.com>
2018-05-31 07:24:59 -0400

    initial commit

--- src/timetracker/main.go
+++ src/timetracker/main.go
@@ -4,23 +4,90 @@ import (
 	"bufio"
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/user"
 	"path/filepath"
+	"sort"
 	"strings"
 	"time"
+
+	"github.com/olekukonko/tablewriter"
 )
 
+const sinceDateFormat = "Jan-02"
+
 func main() {
+
+	yesterday := time.Now().Truncate(24*time.Hour).AddDate(0, 0, -1).Format(sinceDateFormat)
+
+	print := flag.Bool("print", false, "Prints out records in the database")
+	since := flag.String("since", yesterday, "Limits by date the records to print [only applies when -print is enabled]")
+
+	flag.Parse()
 
 	configFile := resolveFileName("~/.timetrack.json")
 	cfg, err := loadConfig(configFile)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	if *print {
+		printRecords(since, cfg)
+		return
+	}
+
+	track(configFile, cfg)
+}
+
+func printRecords(since *string, cfg *Config) {
+
+	tm, err := time.Parse(sinceDateFormat, *since)
+	if err != nil {
+		log.Printf("Date should be in format %s\n", sinceDateFormat)
+		log.Fatal(err)
+	}
+
+	tm = tm.Truncate(24 * time.Hour)
+
+	// add year component b/c it wasn't parsed
+	tm = tm.AddDate(time.Now().Year(), 0, 0)
+
+	fmt.Printf("Showing results since %s\n", tm.Format("2006-Jan-02 15:04"))
+	fmt.Println()
+
+	sort.Sort(byFinishedTime(cfg.Entries))
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Time", "Duration", "Description"})
+
+	for i, entry := range cfg.Entries {
+		if !entry.Finished.After(tm) {
+			continue
+		}
+
+		var lastTime time.Time
+		if i > 0 {
+			lastTime = cfg.Entries[i-1].Finished
+		}
+
+		timeSpent := entry.Finished.Sub(lastTime).Round(time.Minute)
+		timeSpentString := timeSpent.String()
+		if timeSpent > 8*time.Hour {
+			timeSpentString = "-"
+		}
+
+		finished := entry.Finished.Format("Jan-02 15:04")
+		table.Append([]string{finished, timeSpentString, entry.Description})
+	}
+
+	table.Render()
+}
+
+func track(configFile string, cfg *Config) {
 
 	lastTime := cfg.GetLastEntryTime()
 	fmt.Printf("%s since your last entry\n", time.Now().Sub(lastTime).String())
@@ -35,7 +102,7 @@ func main() {
 		Description: desc,
 	})
 
-	err = writeConfig(configFile, cfg)
+	err := writeConfig(configFile, cfg)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -76,6 +143,12 @@ func (c *Config) GetLastEntryTime() time.Time {
 
 	return maxTime
 }
+
+type byFinishedTime []*Entry
+
+func (a byFinishedTime) Len() int           { return len(a) }
+func (a byFinishedTime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byFinishedTime) Less(i, j int) bool { return a[i].Finished.Before(a[j].Finished) }
 
 type Entry struct {
 	Finished    time.Time
